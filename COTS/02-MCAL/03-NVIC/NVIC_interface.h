145e9131ee51b72f3b0d9a1ab6fd9792ee93a6fe
Mahmoud Abdelraouf Mahmoud <mahmoud126125@gmail.com>
2023-06-25 19:47:19 +0300

    Update NVIC Driver

--- COTS/02-MCAL/03-NVIC/NVIC_interface.h
+++ COTS/02-MCAL/03-NVIC/NVIC_interface.h
@@ -19,7 +19,7 @@
  *
  * @retval 1 An error occurred (invalid interrupt number).
  */
-u8 MNVIC_voidEnableInterrupt(u8 Copy_u8InterruptNumber);
+u8 MNVIC_u8EnableInterrupt(u8 Copy_u8InterruptNumber);
 /**
  * @brief Disables a specific interrupt in the Nested Vectored Interrupt Controller (NVIC).
  *
@@ -31,9 +31,45 @@ u8 MNVIC_voidEnableInterrupt(u8 Copy_u8InterruptNumber);
  *
  * @retval 1 An error occurred (invalid interrupt number).
  */
-u8 MNVIC_voidDisableInterrupt(u8 Copy_u8InterruptNumber);
-
-
+u8 MNVIC_u8DisableInterrupt(u8 Copy_u8InterruptNumber);
+/**
+ * @brief Sets a pending flag for a specific interrupt in the Nested Vectored Interrupt Controller (NVIC).
+ *
+ * This function sets a pending flag for a specific interrupt in the NVIC by setting the corresponding bit(s) in the NVIC_ISPRx registers.
+ *
+ * @param[in] Copy_u8InterruptNumber The interrupt number (0-63) to set the pending flag for.
+ * 
+ * @retval 0 No error occurred.
+ * 
+ * @retval 1 An error occurred (invalid interrupt number).
+ */
+u8 MNVIC_u8SetPendingFlag(u8 Copy_u8InterruptNumber);
+/**
+ * @brief Clears a pending flag for a specific interrupt in the Nested Vectored Interrupt Controller (NVIC).
+ * 
+ * This function clears a pending flag for a specific interrupt in the NVIC by clearing the corresponding bit(s) in the NVIC_ICPRx registers.
+ *
+ * @param[in] Copy_u8InterruptNumber The interrupt number (0-63) to clear the pending flag for.
+ * 
+ * @retval 0 No error occurred.
+ *
+ * @retval 1 An error occurred (invalid interrupt number).
+ */
+u8 MNVIC_u8ClearPendingFlag(u8 Copy_u8InterruptNumber); 
+/**
+ * @brief Gets the active flag for a specific interrupt in the Nested Vectored Interrupt Controller (NVIC).
+ * 
+ * This function gets the active flag for a specific interrupt in the NVIC by reading the corresponding bit(s) in the NVIC_IABRx registers.
+ *
+ * @param[in] Copy_u8InterruptNumber The interrupt number (0-63) to get the active flag for.
+ * 
+ * @param[out] Copy_pu8ReturnValue Pointer to a variable to store the active flag value. 1 if active, 0 if not active.
+ * 
+ * @retval 0 No error occurred.
+ *
+ * @retval 1 An error occurred (invalid interrupt number).
+ */
+u8 MNVIC_u8GetActiveFlag(u8 Copy_u8InterruptNumber, u8 *Copy_pu8ReturnValue); 
 
 
 #endif /**< __NVIC_INTERFACE_H__ */
--- COTS/02-MCAL/03-NVIC/NVIC_program.c
+++ COTS/02-MCAL/03-NVIC/NVIC_program.c
@@ -12,7 +12,7 @@
 #include "NVIC_interface.h"
 #include "NVIC_config.h"
 
-u8 MNVIC_voidEnableInterrupt(u8 Copy_u8InterruptNumber) 
+u8 MNVIC_u8EnableInterrupt(u8 Copy_u8InterruptNumber) 
 {
 	u8 Local_u8ErrorStatus = 0;
 	if(Copy_u8InterruptNumber < 32)
@@ -26,13 +26,13 @@ u8 MNVIC_voidEnableInterrupt(u8 Copy_u8InterruptNumber)
 	}
 	else
 	{
-		Local_u8ErrorStatus = 1;		/**< RETURN ERROR STATUS */
+		Local_u8ErrorStatus = 1;		
 	}
 	return Local_u8ErrorStatus;
 }
 
 
-u8 MNVIC_voidDisableInterrupt(u8 Copy_u8InterruptNumber) 
+u8 MNVIC_u8DisableInterrupt(u8 Copy_u8InterruptNumber) 
 {
 	u8 Local_u8ErrorStatus = 0;
 	if(Copy_u8InterruptNumber < 32)
@@ -46,7 +46,64 @@ u8 MNVIC_voidDisableInterrupt(u8 Copy_u8InterruptNumber)
 	}
 	else
 	{
-		Local_u8ErrorStatus = 1;		/**< RETURN ERROR STATUS */
+		Local_u8ErrorStatus = 1;		
+	}
+	return Local_u8ErrorStatus;
+}
+
+u8 MNVIC_u8SetPendingFlag(u8 Copy_u8InterruptNumber) 
+{
+	u8 Local_u8ErrorStatus = 0;
+	if(Copy_u8InterruptNumber < 32)
+	{
+		NVIC_ISPR0 = (1 << Copy_u8InterruptNumber);
+	}
+	else if(Copy_u8InterruptNumber < 64)
+	{
+		Copy_u8InterruptNumber -= 32;
+		NVIC_ISPR1 = (1 << Copy_u8InterruptNumber);
+	}
+	else
+	{
+		Local_u8ErrorStatus = 1;		
+	}
+	return Local_u8ErrorStatus;
+}
+
+u8 MNVIC_u8ClearPendingFlag(u8 Copy_u8InterruptNumber) 
+{
+	u8 Local_u8ErrorStatus = 0;
+	if(Copy_u8InterruptNumber < 32)
+	{
+		NVIC_ICPR0 = (1 << Copy_u8InterruptNumber);
+	}
+	else if(Copy_u8InterruptNumber < 64)
+	{
+		Copy_u8InterruptNumber -= 32;
+		NVIC_ICPR1 = (1 << Copy_u8InterruptNumber);
+	}
+	else
+	{
+		Local_u8ErrorStatus = 1;		
+	}
+	return Local_u8ErrorStatus;
+}
+
+u8 MNVIC_u8GetActiveFlag(u8 Copy_u8InterruptNumber, u8 *Copy_pu8ReturnValue) 
+{
+	u8 Local_u8ErrorStatus = 0;
+	if(Copy_u8InterruptNumber < 32)
+	{
+		*Copy_pu8ReturnValue = GET_BIT(NVIC_IABR0, Copy_u8InterruptNumber);
+	}
+	else if(Copy_u8InterruptNumber < 64)
+	{
+		Copy_u8InterruptNumber -= 32;
+		*Copy_pu8ReturnValue = GET_BIT(NVIC_IABR1, Copy_u8InterruptNumber);
+	}
+	else
+	{
+		Local_u8ErrorStatus = 1;		
 	}
 	return Local_u8ErrorStatus;
 }
