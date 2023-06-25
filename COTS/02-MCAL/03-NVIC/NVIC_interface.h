9fb70a6f6887c9c3917bd9ad6d03bc7160b517c0
Mahmoud Abdelraouf Mahmoud <mahmoud126125@gmail.com>
2023-06-25 17:05:38 +0300

    Update

--- COTS/02-MCAL/03-NVIC/NVIC_interface.h
+++ COTS/02-MCAL/03-NVIC/NVIC_interface.h
@@ -8,8 +8,30 @@
 #define __NVIC_INTERFACE_H__
 
 
-
-
+/**
+ * @brief Enables a specific interrupt in the Nested Vectored Interrupt Controller (NVIC).
+ *
+ * This function enables a specific interrupt in the NVIC by setting the corresponding bit(s) in the NVIC_ISERx registers.
+ *
+ * @param[in] Copy_u8InterruptNumber The interrupt number (0-63) to be enabled.
+ *
+ * @retval 0 No error occurred.
+ *
+ * @retval 1 An error occurred (invalid interrupt number).
+ */
+u8 MNVIC_voidEnableInterrupt(u8 Copy_u8InterruptNumber);
+/**
+ * @brief Disables a specific interrupt in the Nested Vectored Interrupt Controller (NVIC).
+ *
+ * This function disables a specific interrupt in the NVIC by clearing the corresponding bit(s) in the NVIC_ICERx registers.
+ *
+ * @param[in] Copy_u8InterruptNumber The interrupt number (0-63) to be disabled.
+ * 
+ * @retval 0 No error occurred.
+ *
+ * @retval 1 An error occurred (invalid interrupt number).
+ */
+u8 MNVIC_voidDisableInterrupt(u8 Copy_u8InterruptNumber);
 
 
 
--- COTS/02-MCAL/03-NVIC/NVIC_program.c
+++ COTS/02-MCAL/03-NVIC/NVIC_program.c
@@ -12,4 +12,41 @@
 #include "NVIC_interface.h"
 #include "NVIC_config.h"
 
-	MNVIC
+u8 MNVIC_voidEnableInterrupt(u8 Copy_u8InterruptNumber) 
+{
+	u8 Local_u8ErrorStatus = 0;
+	if(Copy_u8InterruptNumber < 32)
+	{
+		NVIC_ISER0 = (1 << Copy_u8InterruptNumber);
+	}
+	else if(Copy_u8InterruptNumber < 64)
+	{
+		Copy_u8InterruptNumber -= 32;
+		NVIC_ISER1 = (1 << Copy_u8InterruptNumber);
+	}
+	else
+	{
+		Local_u8ErrorStatus = 1;		/**< RETURN ERROR STATUS */
+	}
+	return Local_u8ErrorStatus;
+}
+
+
+u8 MNVIC_voidDisableInterrupt(u8 Copy_u8InterruptNumber) 
+{
+	u8 Local_u8ErrorStatus = 0;
+	if(Copy_u8InterruptNumber < 32)
+	{
+		NVIC_ICER0 = (1 << Copy_u8InterruptNumber);
+	}
+	else if(Copy_u8InterruptNumber < 64)
+	{
+		Copy_u8InterruptNumber -= 32;
+		NVIC_ICER1 = (1 << Copy_u8InterruptNumber);
+	}
+	else
+	{
+		Local_u8ErrorStatus = 1;		/**< RETURN ERROR STATUS */
+	}
+	return Local_u8ErrorStatus;
+}
